cd8689e32294806641dcf20453e9bb5044857de3
Rabiul Islam <52354711+rabi993@users.noreply.github.com>
2023-09-09 16:24:19 +0600

    question invisible to visible . load quiz okay

--- index.js
+++ index.js
@@ -13,6 +13,37 @@ let quizContainer = document.querySelector("#quizContainer");
 let answersContainer = document.querySelector("#answersContainer");
 let displayResult = document.querySelector("#displayResult");
 
+
+
+// All quiz data fetched from json
+const loadQuiz = async () => {
+  const res = await fetch("./data/quiz.json");
+  const data = await res.json();
+  displayQuiz(data);
+  quizData = data;
+};
+
+// Displaying quiz on quiz page
+const displayQuiz = (data) => {
+  if (!data) {
+    quizContainer.innerHTML = "";
+    return;
+  }
+
+  data.forEach((quiz, i) => {
+    quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
+  <div class="flex items-center">
+    <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
+      ${i + 1}
+    </div>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
+  </div>
+  <div class="grid grid-cols-2 gap-4 mt-5">
+    ${displayQuizOptions(quiz.options, i)}
+  </div>
+</div>`;
+  });
+};
 // EventListener for quiz start button
 startQuiz.addEventListener("click", () => {
   let countDown = document.querySelector("#countDownContainer");
@@ -43,35 +74,7 @@ startQuiz.addEventListener("click", () => {
   }, 1000);
 });
 
-// All quiz data fetched from json
-const loadQuiz = async () => {
-  const res = await fetch("./data/quiz.json");
-  const data = await res.json;
-  quizData = data;
-  displayQuiz(data);
-};
 
-// Displaying quiz on quiz page
-const displayQuiz = (data) => {
-  if (!data) {
-    quizContainer.innerHTML = "";
-    return;
-  }
-
-  data.forEach((quiz, i) => {
-    quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
-  <div class="flex items-center">
-    <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
-      ${i + 1}
-    </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
-  </div>
-  <div class="grid grid-cols-2 gap-4 mt-5">
-    ${displayQuizOptions(quiz.options, i)}
-  </div>
-</div>`;
-  });
-};
 
 // EventListener for quiz submit button
 document.querySelector("#submit").addEventListener("click", () => {
